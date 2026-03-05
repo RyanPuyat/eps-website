5d29d726fec2f8f9c720e246159ffe5dff0dba6a
Ryan Puyat <ryanspuyat@gmail.com>
2026-03-05 21:24:40 +0900

    refactor: css color for button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from '@/components/ui/button';
+
 export const metadata = {
   title: 'Home',
 };
 
 function HomePage() {
-  return <div className="text-blue-600">Eps Website</div>;
+  return (
+    <>
+      <div className="text-blue-600">Eps Website</div>
+      <Button variant="success">Green Button</Button>
+    </>
+  );
 }
 
 export default HomePage;
