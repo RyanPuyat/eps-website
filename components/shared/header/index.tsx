2f0edb423364d9782154bdb48451c803a12f129c
Ryan Puyat <ryanspuyat@gmail.com>
2026-03-07 22:57:34 +0900

    feat: Add footer component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,4 +1,4 @@
-import { UserIcon } from 'lucide-react';
+import { User2Icon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
@@ -13,10 +13,21 @@ function Header() {
             <Image
               src="/images/logo.png"
               alt={`${APP_NAME} logo`}
-              height={80}
-              width={80}
+              height={50}
+              width={50}
+              priority={true}
             />
+            <span className="hidden lg:block font-bold text-2xl ml-3 app-name tracking-tighter">
+              {APP_NAME}
+            </span>
           </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <User2Icon /> Sign In
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'EPS Corner';
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'EPS CORNER';
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Website built for EPS aspirant';
 export const SERVER_URL =
