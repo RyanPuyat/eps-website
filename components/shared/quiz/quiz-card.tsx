cad1090bbc174d03599a4ad8d90242a3ff9d6b7c
Ryan Puyat <ryanspuyat@gmail.com>
2026-03-12 23:43:48 +0900

    feat: Add quiz section style

--- components/shared/quiz/quiz-card.tsx
+++ components/shared/quiz/quiz-card.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Link from 'next/link';
 
 function QuizCard() {
@@ -6,12 +6,28 @@ function QuizCard() {
     <>
       <section className="wrapper my-10">
         <h2 className="h2-bold mb-4">Quiz</h2>
-        <div className="grid grid-cols-1 sm-grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <Card className="w-full max-w-sm">
-            <CardHeader className="p-0 items-center">
-              <Link href="/quiz">To Quiz</Link>
-            </CardHeader>
-          </Card>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center">
+          <Link href="/quiz" className="card-wrapper variant-rotate">
+            <Card className="relative z-10 h-full w-full bg-background">
+              <CardHeader className="h2-bold justify-items-center">
+                <CardTitle>KLT EXAM</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>This card now has a conic-gradient rotating behind it.</p>
+              </CardContent>
+            </Card>
+          </Link>
+
+          <Link href="/quiz" className="card-wrapper variant-rotate-alt">
+            <Card className="relative z-10 h-full w-full bg-background">
+              <CardHeader className="h2-bold justify-items-center">
+                <CardTitle>KLT EXAM</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>This card now has a conic-gradient rotating behind it.</p>
+              </CardContent>
+            </Card>
+          </Link>
         </div>
       </section>
     </>
