9462373ab241690ca63a68a7ed12d65fe17cdb74
Ryan Puyat <ryanspuyat@gmail.com>
2026-03-13 22:08:36 +0900

    refactor: change logo

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,11 +1,18 @@
-import Logo from './logo';
+// import Logo from './logo';
+import Link from 'next/link';
 import Menu from './menu';
 function Header() {
   return (
     <div className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Logo />
+          {/* <Logo /> */}
+          <Link href="/">
+            <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl h2-bold text-primary transition-all hover:translate-y-0.5 md:block dark:border-white mr-2">
+              E
+            </p>
+          </Link>
+          <h2 className="h2-bold text-primary">PS CORNER</h2>
         </div>
 
         <Menu />
