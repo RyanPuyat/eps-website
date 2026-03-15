07be984413616025a29a587a4ddf48af0c7dea7b
Ryan Puyat <ryanspuyat@gmail.com>
2026-03-15 22:25:04 +0900

    feat: Add hero video

--- components/shared/hero/hero.tsx
+++ components/shared/hero/hero.tsx
@@ -4,14 +4,24 @@ function Hero() {
   return (
     <section className="w-full flex items-center justify-center">
       <div className="wrapper max-w-6xl mx-auto px-4 my-10">
-        <h1 className="text-5xl font-bold text-center">
-          Welcome to My Hero Section
-        </h1>
-        <p className="mt-4 text-lg text-center text-gray-600">
-          This is a full-width hero with a centered wrapper.
-        </p>
-        {/* <Button>Get Started</Button> */}
-        {/* <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700"></button> */}
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
+          <div className="flex-1 text-center md:text-left">
+            <h1 className="text-5xl font-bold text-center">
+              Welcome to My Hero Section
+            </h1>
+            <p className="mt-4 text-lg text-center text-gray-600">
+              This is a full-width hero with a centered wrapper.
+            </p>
+          </div>
+          <div className="mt-8">
+            <video className="mx-auto" width="600" autoPlay loop muted>
+              <source src="/videos/cube.webm" type="video/webm" />
+              {/* Fallback for browsers that don’t support .webm */}
+              {/* <source src="/videos/my-hero-video.mp4" type="video/mp4" /> */}
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
       </div>
     </section>
   );
